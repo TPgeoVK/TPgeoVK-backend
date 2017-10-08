27d017625e7e7684bd70010e4fa758fa7e6fc59f
TimeEscaper <rentgeny05@gmail.com>
2017-10-08 06:38:44 +0300

    Script fixed

--- before_install.sh
+++ before_install.sh
@@ -4,4 +4,5 @@ openssl aes-256-cbc -K $encrypted_f955482a5e6e_key -iv $encrypted_f955482a5e6e_i
 rm travis_rsa.enc
 chmod 600 travis_rsa
 mv travis_rsa ~/.ssh/travis_rsa
+mv ssh_config ~/.ssh/config
 
