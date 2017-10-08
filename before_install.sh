9cfc7daf3a353543a02d7b0e4012655f8b5443c4
TimeEscaper <rentgeny05@gmail.com>
2017-10-08 06:42:42 +0300

    Script fixed

--- before_install.sh
+++ before_install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -x
 openssl aes-256-cbc -K $encrypted_f955482a5e6e_key -iv $encrypted_f955482a5e6e_iv -in travis_rsa.enc -out travis_rsa -d
+touch ~/.ssh/known_hosts
 rm travis_rsa.enc
 chmod 600 travis_rsa
 mv travis_rsa ~/.ssh/travis_rsa
