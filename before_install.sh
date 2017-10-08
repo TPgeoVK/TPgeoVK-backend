3d4d715c47fa3a57d1998ae258f93c2ad2e3be57
TimeEscaper <rentgeny05@gmail.com>
2017-10-08 07:08:26 +0300

    .travis.yml fixed

--- before_install.sh
+++ before_install.sh
@@ -3,6 +3,6 @@ set -x
 openssl aes-256-cbc -K $encrypted_f955482a5e6e_key -iv $encrypted_f955482a5e6e_iv -in travis_rsa.enc -out travis_rsa -d
 rm travis_rsa.enc
 chmod 600 travis_rsa
-mv travis_rsa ~/.ssh/travis_rsa
+mv travis_rsa ~/.ssh/id_rsa
 mv ssh_config ~/.ssh/config
 
