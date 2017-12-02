366c3b302f72f188ba0643138bd01931d987f2d6
TimeEscaper <rentgeny05@gmail.com>
2017-12-02 19:52:10 +0300

    Nearest places reduced

--- src/main/java/ru/tpgeovk/back/service/LocationService.java
+++ src/main/java/ru/tpgeovk/back/service/LocationService.java
@@ -234,6 +234,10 @@ public class LocationService {
             return new ArrayList<>();
         }
 
+        if (nearestPlaces.size() > 15) {
+            nearestPlaces = nearestPlaces.subList(0, 15);
+        }
+
         return nearestPlaces.stream()
                 .map(a -> FullPlaceInfo.fromPlaceFull(a))
                 .collect(Collectors.toList());
